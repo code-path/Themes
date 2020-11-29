f212a61a1441e337239c5dce1bbf2df3d113bc5e
code-path <43168059+code-path@users.noreply.github.com>
2020-11-29 20:17:48 +0330

    Resolve error 1009
    remove domain lock

--- application/themes/auzwow/js/footer_include.js
+++ application/themes/auzwow/js/footer_include.js
@@ -57,38 +57,6 @@ function CustomJS()
 
     this._initialize = function()
     {
-        if(!$('.footer').length || !$('.footer .row-2').length || !$('.darksider').length || $('.darksider').is(':empty') || !$('.darksider a:contains("E. Darksider")').length)
-        {
-            $('.darksider').remove();
-            $('.footer .row-2 .col-1').append('<span class="darksider">Coded by <a href="skype:darkstriders?chat">E. Darksider</a></span>');
-        }
-
-        Homie = {
-            name: location.hostname.replace('www.', ''),
-            tld: location.hostname.split('.').slice(-1)[0],
-            protocol: location.protocol,
-            address: location.hostname
-        };
-
-        if(Homie['name'] === '127.0.0.1' || location.hostname.split('.').slice(0)[0] === '192' && location.hostname.split('.').slice(0)[1] === '168')
-            return true;
-
-        LegalSHIT = ['localhost', 'homie.org', 'weed.legal', 'shit.gold', '51.255.195.66', '185.2.29.4', 'cataclysm-wow.com', 'project-nightfall.org'];
-
-        for(SHIT in LegalSHIT)
-        {
-            if(Homie['name'] === LegalSHIT[SHIT] || Homie['address'] === LegalSHIT[SHIT])
-                return true;
-        }
-
-        var Base64={_keyStr:'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-
-        var encodedString = Base64.encode(LegalSHIT[LegalSHIT.length - 1]);
-
-        $('html')
-            .html('<div style="width:100%;height:100%;top:0;left:0;padding-top:10%;position:absolute;text-align:center;border-box:box-sizing;font-size:18px;background-color:#a6bdc1;">Error code: 1009<br />Your license has been terminated.<br />Ref ID: ' + encodedString + '<br /><br />Contact <a href="skype:darkstriders?chat">E. Darksider</a> on skype</div>');
-
-        return false;
     }
 
     this.visualizeSelect = function()
